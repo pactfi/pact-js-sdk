795a271e0c774b7c816c82df32ede8364b6ae1e7
Mark McCann <markmccann96@gmail.com>
2022-04-17 20:59:16 +1000

    docs PA-156: Added documentation using Typedoc

---
+++ push-docs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+remoteUrl=$(git remote get-url origin);
+version=$(git describe --tags);
+
+git clone $remoteUrl -b gh-pages gh-pages
+
+npm run docs
+
+mv docs "gh-pages/$version"
+
+cd gh-pages
+
+ln -s "$version" latest
+git add -A
+git commit -m "Added docs for $version"
+git push origin gh-pages
