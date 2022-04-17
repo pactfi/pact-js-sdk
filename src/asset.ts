ed85a52d3548ba48f4b6a3dcf01387c9472448b3
Mark McCann <markmccann96@gmail.com>
2022-04-17 20:59:16 +1000

    docs PA-156: Added documentation using Typedoc

--- src/asset.ts
+++ src/asset.ts
@@ -1,5 +1,16 @@
 import algosdk from "algosdk";
 
+/**
+ * fetchAssetByIndex returns an Asset class with the details about the asset for a given id number.
+ *
+ * The function uses an internal cache so as to minimize the number of times the actual Algorand client is used
+ * to look up the asset. This function is used through out the pact sdk to query asset information.
+ *
+ * @param algod The Algorand client to query about the asset.
+ * @param index The Algorand Asset number to look up.
+ *
+ * @returns An Asset class for the asset number passed in.
+ */
 export async function fetchAssetByIndex(
   algod: algosdk.Algodv2,
   index: number,
@@ -30,21 +41,75 @@ export async function fetchAssetByIndex(
   return asset;
 }
 
+/**
+ * Describes the basic data and the utility functions for an Algorand Standard Asset.
+ *
+ * The class includes basic details of the asset like name, unitName, number of
+ * decimals supported and ratio from base units to unit.
+ *
+ * Typically you don't create instances of this class manually. Use [[PactClient.fetchAsset]] instead.
+ * Also, when instantiating the pool e.g. by using [[PactClient.fetchPoolById]] the missing pool assets are fetched automatically.
+ */
 export class Asset {
+  /**
+   * A cache of the asset index to [[Asset]] to reduce the time for
+   * looking up basic details about the asset.
+   */
   static assetsCache: Record<number, Asset> = {};
 
+  /**
+   * The name of the Asset if there is one. This may be empty.
+   */
   public name? = "";
+  /**
+   * The name of a unit of the asset if there is one. This may be empty.
+   */
   public unitName? = "";
+
+  /**
+   * The number of decimal places that the Asset supports.
+   */
   public decimals = 0;
+
+  /**
+   * The ratio between a base unit and the unit of the asset.
+   * This is used to convert between an integer and floating point
+   * representation of the asset without loss of precision.
+   */
   public ratio = 1;
 
+  /**
+   * Creates an Asset class setting the index and Algorand client.
+   *
+   * Note that clients would not usually call this constructor themselves.
+   *
+   * @param algod the Algorand sdk client to use for extracting asset details.
+   * @param index the index number of the asset.
+   */
   constructor(protected algod: algosdk.Algodv2, public index: number) {}
 
+  /**
+   * This creates a transaction that will allow the account to "opt in" to the asset.
+   *
+   * In Algorand, every account has to explicitly opt-in for an asset before receiving it.
+   *
+   * Needed if you want to receive an asset from a swap or to manage liquidity tokens.
+   *
+   * @param address Account to opt in to this asset.
+   * @returns Transaction for opting in to this asset.
+   */
   async prepareOptInTx(address: string) {
     const suggestedParams = await this.algod.getTransactionParams().do();
     return this.buildOptInTx(address, suggestedParams);
   }
 
+  /**
+   * Creates the actual transaction for the account to opt-in to holding the asset.
+   *
+   * @param address address of the account to opt in to the asset.
+   * @param suggestedParams the general parameters for the transaction based on the particular client
+   * @returns a transaction
+   */
   buildOptInTx(address: string, suggestedParams: algosdk.SuggestedParams) {
     return algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
       from: address,
@@ -55,16 +120,40 @@ export class Asset {
     });
   }
 
+  /**
+   * Checks if the account is already able to hold this asset, that is it has already opted in.
+   *
+   * This functions should be called to check if the opt-in transaction needs to be created. See [prepareOptInTx].
+   *
+   * @param address The account to check if the asset is opted in on.
+   *
+   * @returns True if the account is already opted in, false otherwise.
+   */
   async isOptedIn(address: string): Promise<boolean> {
     const holding = await this.getHolding(address);
     return holding !== null;
   }
 
+  /**
+   * Returns the amount of holding of this asset the account has.
+   *
+   * Note that this function may return null if the account has not opted in for this asset.
+   * @param address The account to check the current holding.
+   *
+   * @returns The amount of this asset the account is holding, or null if it can't hold this asset.
+   */
   async getHolding(address: string): Promise<number | null> {
     const accountInfo = await this.algod.accountInformation(address).do();
     return this.getHoldingFromAccountInformation(accountInfo);
   }
 
+  /**
+   * Used by getHolding to return the actual amount of asset from the accountInformation.
+   *
+   * @param accountInformation The account information to extract the asset holding from.
+   *
+   * @returns The amount of asset or null if the asset is not in the account.
+   */
   getHoldingFromAccountInformation(accountInformation: any) {
     for (const asset of accountInformation.assets) {
       if (asset["asset-id"] === this.index) {
