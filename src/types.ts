9d7f29ef425b124d4cd4e2254a5189eeff1ab540
Mark McCann <markmccann96@gmail.com>
2022-05-04 00:56:05 +1000

    {feat} added doc string for type file.

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,4 @@
+/**
+ * The basic three operation types in a PACT liquidity pool, namely Add Liquidity (ADDLIQ), Remove Liquidity (REMLIQ) and trade a swap.
+ */
 export type OperationType = "SWAP" | "ADDLIQ" | "REMLIQ";
